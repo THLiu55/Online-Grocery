bf1cc67f1899895b27cf486fa7e06650a5fc8ad3
liutianhao <liutianhao328@gmail.com>
2022-09-30 14:05:26 +0800

    login signup backup v0.1

--- blueprint/user_blueprint.py
+++ blueprint/user_blueprint.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from models import User
+from exts import db
 
 
 user_bp = Blueprint("User", __name__, url_prefix="/user")
@@ -19,6 +20,21 @@ def login():
             user = user_tmp
             return redirect(url_for("app.index"))
     return redirect(url_for("login"))
+
+
+@user_bp.route("/register", methods=["GET", "POST"])
+def register():
+    global user
+    username = request.args.get("name")
+    password = request.args.get("password")
+    user_tmp = User.query.filter_by(username=username).first()
+    if user_tmp is None:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('app.index'))
+    else:
+        return redirect(url_for('register'))
 
 
 @user_bp.route("/profile")
