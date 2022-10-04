f5ca2339d3e49d2f67cda7c6ff7597e7b482fddb
liutianhao <liutianhao328@gmail.com>
2022-10-04 22:02:00 +0800

    register add function (send email & generating captcha code)

--- blueprint/user_blueprint.py
+++ blueprint/user_blueprint.py
@@ -15,6 +15,7 @@ user = None
 @user_bp.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        print(2)
         global user
         result = request.form
         email = result['email']
@@ -26,58 +27,69 @@ def login():
                 return render_template('MainpageBase.html')
         return render_template('login.html')
     else:
+        print(3)
         return render_template('login.html')
 
 
-@user_bp.route("/captcha", methods=["POST"])
-def captcha():
-    # here the email format validity has already been checked
-    result = request.form
-    email = result["email"]
-    user_tmp = Customer.query.filter_by(email=email).first() # check if the email has been registered
-    if user_tmp is None:
-        # generate the captcha of the account
-        captcha = ""
-        for i in range(6):
-            captcha += str(random.randint(0, 9))
-        # store the email-captcha pair to database
+@user_bp.route("/captcha/<type>", methods=["GET", "POST"])
+def captcha(email):
+    print(1)
+
+
+
+
+@user_bp.route("/register/<type>", methods=["GET", "POST"])
+def register(type):
+    if type == "1":  # if send email button clicked, go here
+        # get email address
+        email = request.args.get("email")
+        # here the email format validity has already been checked
+        user_tmp = Customer.query.filter_by(email=email).first()  # check if the email has been registered
+        if user_tmp is None:
+            # generate the captcha of the account
+            captcha = ""
+            for i in range(6):
+                captcha += str(random.randint(0, 9))
+            # store the email-captcha pair to database
+            pair = Captcha.query.filter_by(email=email).first()
+            if pair is None:  # no pair -> init a new pair
+                pair = Captcha(email=email, captcha=captcha, create_time=datetime.now())
+                db.session.add(pair)
+                db.session.commit()
+            else:  # have old pair -> update captcha & create time
+                pair.captcha = captcha
+                pair.create_time = datetime.now()
+                db.session.commit()
+            # send email to user's email address
+            message = Message(
+                subject="Welcome to register GoGrocery",
+                recipients=[email],
+                html=render_template('email.html', captcha=captcha)
+            )
+            mail_sender.send(message)
+            return {'code': 200}
+        return {'code': 400, 'message': "registered email"}
+
+    else:  # confirm register clicked, go here
+        form = request.form
+        # if the computer goes here, the format validation has all been checked
+        username = form.get("username")
+        password = form.get("password")
+        email = form.get("email")
+        captcha = form.get("captcha")
+        user_tmp = Customer.query.filter_by(username=username).first()
         pair = Captcha.query.filter_by(email=email).first()
-        if pair is None:  # no pair -> init a new pair
-            pair = Captcha(email=email, captcha=captcha, create_time=datetime.now())
-            db.session.add(pair)
+        if (user_tmp is None) and (pair is not None):
+            if (datetime.now() - pair.create_time).seconds > 1800:
+                return {'code': 400, 'message': "captcha out of time"}
+            if pair.captcha == captcha:
+                return {'code': 400, 'message': "captcha wrong"}
+            new_customer = Customer(password=password, username=username, email=email)
+            db.session.add(new_customer)
             db.session.commit()
-        else:  # have old pair -> update captcha & create time
-            pair.captcha = captcha
-            pair.create_time = datetime.now()
-            db.session.commit()
-        # send email to user's email address
-        message = Message(
-            subject="Welcome to register GoGrocery",
-            recipients=[email],
-            html=render_template('email.html', captcha=captcha)
-        )
-        mail_sender.send(message)
-        return {'code': 200}
-    return {'code': 400, 'message': "registered email"}
+            return {'code': 200}
+        return {'code': 400, 'message': "registered username"}
 
-
-
-@user_bp.route("/register", methods=["GET", "POST"])
-def register():
-    global user
-    result = request.form
-    username = result['username']
-    email = result['email']
-    password = result['password']
-    validation = result['validation']
-    user_tmp = Customer.query.filter_by(email=email).first()
-    if user_tmp is None:
-        user = Customer(userName=username, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('index'))
-    else:
-        return redirect(url_for('register'))
 
 
 @user_bp.route("/profile")
