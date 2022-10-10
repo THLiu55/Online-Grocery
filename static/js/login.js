dc7bb9b06a859a35b0b8c66fcb6430ed9b19cfc4
Tianhao Liu <tianhao.liu@ucdconnect.ie>
2022-10-10 09:50:59 +0800

    all input format validation

--- static/js/login.js
+++ static/js/login.js
@@ -3,11 +3,23 @@ const to_sign_in_btn = document.getElementById("to-logIn");
 const to_sign_up_btn = document.getElementById("to-signUp");
 const container = document.getElementsByClassName("Component")[0];
 
-// input
+// error message displayer
 const email_error = document.getElementById("email_error");
-const num_div = document.getElementById('count')
 const username_error = document.getElementById("username_error");
 const captcha_error = document.getElementById("captcha_error");
+const password_error = document.getElementById("password_error");
+const repassword_error = document.getElementById("Repassword_error");
+
+// countdown displayer
+const num_div = document.getElementById('count')
+
+// input
+const email_input = document.getElementById('email_input')
+const username_input = document.getElementById("username_input")
+const password_input = document.getElementById("password_input")
+const repassword_input = document.getElementById("repassword_input")
+const captcha_input = document.getElementById("captcha_input")
+
 
 // boolean value that represent each input is valid
 let validUserName = false, validPassword = false, validRePassword = false, validEmail = false, validCaptcha = false
@@ -27,7 +39,7 @@ to_sign_in_btn.addEventListener("click", () => {
 // send email
 function send_email() {
     // check if is empty
-    let email = document.getElementById('email_input').value
+    let email = email_input.value
     if (email === '') {
         email_error.innerHTML = "empty email"
         return;
@@ -58,6 +70,39 @@ function send_email() {
 }
 
 function register() {
+    // check empty
+    let flag = true
+
+    if (username_input.value === '') {
+        username_error.innerHTML = 'empty user name'
+        flag = false;
+    }
+    if (password_input.value === '') {
+        password_error.innerHTML = 'empty password'
+        flag = false;
+    }
+    if (repassword_input.value === '') {
+        repassword_error.innerHTML = 'empty password'
+        flag = false;
+    }
+    if (email_input.value === '') {
+        email_error.innerHTML = 'empty email'
+        flag = false;
+    }
+    if (captcha_input.value === '') {
+        captcha_error.innerHTML = 'empty captcha'
+        flag = false;
+    }
+    if (!flag) return;
+
+    // check data validation:
+    if (validUserName) {
+        username_error.innerHTML = "3 <= username length <= 20"
+        flag = false;
+    }
+
+
+
     // send registered form to flask backend
     let xhr = new XMLHttpRequest()
     const fd = new FormData(document.form2)
@@ -89,8 +134,9 @@ function email_listener() {
     // clear the error message
     email_error.innerHTML = "";
     // obtain input email
-    let email = document.getElementById('email_input').value
+    let email = email_input.value
     console.log(email)
+    // check if the email is in correct format (aaa@bbb.ccc)
     if (reg.test(email)) {
         validEmail = true
     } else {
@@ -99,11 +145,89 @@ function email_listener() {
 }
 
 function username_listener() {
+    // clear error message
     email_error.innerHTML = "";
+    // obtain user name
+    let username = username_input.value
+    // check if username is valid (3 < length < 20)
+    if (username.length > 3 && username.length < 20) {
+        validUserName = true;
+    } else {
+        validUserName = false;
+    }
 }
 
 function captcha_listener() {
     email_error.innerHTML = "";
+    // obtain captcha
+    let captcha = captcha_input.value
+    // check if captcha is valid (int(0-9) * 6)
+    for (let i = 0; i < captcha.length; i++) {
+        if (i === 6 || captcha[i] < '0' || captcha[i] > '9') {
+            validCaptcha = false;
+            return;
+        }
+    }
+    validCaptcha = true
+}
+
+function password_listener() {
+    // get password
+    password_error.innerHTML = ""
+    let hasLetter = false, hasNum = false, hasCap = false
+    let password = password_input.value
+    let len = password.length
+
+    // check length
+    if (len < 6) {
+        password_error.innerHTML = "too short"
+        validPassword = false
+        return;
+    }
+    if (len > 20) {
+        password_error.innerHTML = "too long"
+        validPassword = false
+        return;
+    }
+
+    // check format
+    for (let i = 0; i < password.length; i++) {
+        if ('0' <= password[i] <= '9') {
+            hasNum = true
+        } else if ('a' <= password[i] <= 'z' || 'A' <= password[i] <= 'Z') {
+            hasLetter = true
+            if ('A' <= password[i] <= 'Z') {
+                hasCap = true
+            }
+        }
+        // if all satisfied -> return
+        if (hasNum && hasLetter && hasCap && (6 <= len <= 20)) {
+            validPassword = true;
+            return;
+        }
+    }
+    // show error message and set valid to false
+    if (!hasLetter) {
+        password_error.innerHTML = "weak (need letters)"
+        validPassword = false
+        return;
+    }
+    if (!hasNum) {
+        password_error.innerHTML = "weak (need number)"
+        validPassword = false
+        return;
+    }
+    if (!hasCap) {
+        password_error.innerHTML = "weak (need capital letter)"
+        validPassword = false
+    }
+}
+
+// check if re-input password == password
+function rePassword_listener() {
+    let pass = password_input.value
+    let repass = repassword_input.value
+    validRePassword = (pass === repass)
 }
 
 function login() {
