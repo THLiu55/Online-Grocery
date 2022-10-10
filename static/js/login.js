2fd624238ef664da79721c4465053d4ca49b4e3e
Grey Xiao <zhiyou.xiao@ucdconnect.ie>
2022-10-10 16:52:21 +0800

    modification

--- static/js/login.js
+++ static/js/login.js
@@ -317,5 +317,45 @@ back_to_main.addEventListener("click", function (event){
     }, .2)
 })
 
+//the 'continue' logic in forget password framework
+//-- the initial setting of email, captcha and submit area
+    var EmailArea = document.getElementById("email-area");
+    var CaptchaArea = document.getElementById("captcha-area");
+    var PasswordArea = document.getElementById("password-area");
+    setTimeout(function (){
+        EmailArea.style.opacity = '1';
+    }, .2)
+    setTimeout(function (){
+        CaptchaArea.style.opacity = '0';
+    }, .2)
+    setTimeout(function (){
+        PasswordArea.style.opacity = '0';
+    }, .2)
+function email_continue(){
+    var EmailArea = document.getElementById("email-area");
+    var CaptchaArea = document.getElementById("captcha-area");
+
+    EmailArea.style.display = 'none';
+    CaptchaArea.style.display = 'block';
+    setTimeout(function (){
+        CaptchaArea.style.opacity = '1';
+    }, .2)
+    setTimeout(function (){
+        EmailArea.style.opacity = '0';
+    }, .2)
+}
+function captcha_continue(){
+    var CaptchaArea = document.getElementById("captcha-area");
+    var PasswordArea = document.getElementById("password-area");
+
+    CaptchaArea.style.display = 'none';
+    PasswordArea.style.display = 'block';
+    setTimeout(function (){
+        PasswordArea.style.opacity = '1';
+    }, .2)
+    setTimeout(function (){
+        CaptchaArea.style.opacity = '0';
+    }, .2)
+}
 
 
