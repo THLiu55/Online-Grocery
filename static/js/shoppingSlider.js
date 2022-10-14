d4f38a3246ff15bd236ff6360460eb376204895f
Grey Xiao <zhiyou.xiao@ucdconnect.ie>
2022-10-14 23:08:50 +0800

    add js to align

--- static/js/shoppingSlider.js
+++ static/js/shoppingSlider.js
@@ -11,3 +11,21 @@ btn_left.addEventListener("click", function (){
 btn_right.addEventListener("click", function (){
     slider.scrollLeft += 125;
 });
+
+// set the margin of div of shopping cart
+// -- get a single <li>
+var single_li = document.getElementsByClassName("card-item")[0];
+// -- get the width of a single <li>
+var single_li_length = window.getComputedStyle(single_li).width;
+// -- get the margin of a single <li>
+var single_li_margin = window.getComputedStyle(single_li).margin;
+// -- compute the total length of <li>s in a line
+var total_length = 4 * (parseInt(single_li_length) + 2 * parseInt(single_li_margin));
+// -- get the width of the window
+var window_width = window.screen.width;
+// -- compute the margin
+var margin_cart = window_width - total_length;
+// set the margin
+var cart_area = document.getElementsByClassName("shopping-cart-present")[0];
+cart_area.style.marginLeft = margin_cart + 'px';
+cart_area.style.marginRight = margin_cart + 'px';
