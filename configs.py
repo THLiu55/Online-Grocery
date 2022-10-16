db937d3c9111f60dcc201b2ad40a548cc7fa029b
Grey Xiao <zhiyou.xiao@ucdconnect.ie>
2022-10-16 21:51:06 +0800

    debug v2

--- configs.py
+++ configs.py
@@ -1,12 +1,6 @@
 # database configurations
-HOSTNAME = '127.0.0.1'
-PORT     = '3306'
-DATABASE = 'online_shop'
-USERNAME = 'root'
-PASSWORD = 'Xzy20011224'
-DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+DB_URI = 'sqlite:////Users/grey/Documents/gogrocery/online_shop.db'
 SQLALCHEMY_DATABASE_URI = DB_URI
-SECRET_KEY = 'qr9ur34567823disdha3'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
