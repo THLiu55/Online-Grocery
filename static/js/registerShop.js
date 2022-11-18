2087f668af3ca65d3ee1139fb30df2cd687fd69a
Grey Xiao <zhiyou.xiao@ucdconnect.ie>
2022-11-18 17:44:53 +0800

    polish style of pages and add uploading picture functionality

--- static/js/registerShop.js
+++ static/js/registerShop.js
@@ -2,6 +2,23 @@
 const register_shop_btn = document.getElementById("shop-register-btn");
 const profile_back_btn = document.getElementById("profile-back-btn");
 
+// get picture input
+const picture_input_file = document.getElementById("shop-logo-input");
+
+picture_input_file.addEventListener("change", function () {
+    // when uploading picture, we get picture file
+    const { files } = this;
+    // get the first file
+    if (files.length >= 2){
+        files[0].remove();
+    }
+    const pic = files[0];
+    // preview this picture
+    const pic_preview = document.getElementById("preview-pic");
+    pic_preview.src = URL.createObjectURL(pic);
+
+})
+
 register_shop_btn.addEventListener("click", function () {
     let register_frame = document.getElementsByClassName("register-component")[0];
     register_frame.style.visibility = `visible`;
