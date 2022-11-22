65f5b0ab547e30e90c9b4dabd78193511e5271d2
Tianhao Liu <tianhao.liu@ucdconnect.ie>
2022-11-22 08:56:54 +0800

    save new product information to database

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,8 +1,26 @@
 function registerShop() {
     let xhr = new XMLHttpRequest()
-    xhr.open('POST', '/profile/', true)
+    xhr.open('POST', '/profile?type=register', true)
     xhr.setRequestHeader("X-CSRFToken", "{{ register_form.csrf_token._value() }}")
     let form = document.getElementById("shop-register-form")
+    const fd = new FormData(form)
+    xhr.send(fd)
+
+    xhr.onload = function () {
+        let response = JSON.parse(xhr.responseText)
+        if (response.code === 400) {
+            alert(response.message)
+        } else {
+            window.open("/profile")
+        }
+    }
+}
+
+function addNewProduct() {
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', '/profile?type=newProduct', true)
+    xhr.setRequestHeader("X-CSRFToken", "{{ register_form.csrf_token._value() }}")
+    let form = document.getElementById("products-adding-form")
     const fd = new FormData(form)
     xhr.send(fd)
 
